a8ead5f672cf4fbf6d4e3c451f700a29b0ecd993
Sny <semidaandreicha@gmail.com>
2019-01-17 22:18:56 +0000

    #Broken down Algorithm

--- V2-Parallel.py
+++ V2-Parallel.py
@@ -6,6 +6,7 @@ def transform(self, f):
     return f(self)
 DataFrame.transform = transform
 if __name__ == '__main__':
+    #initializing spark objects
     spark = SparkSession.builder.master("local[*]").appName("University Ranking").getOrCreate()
     sparkContext = spark.sparkContext
     #importing/reading all csv files
@@ -17,7 +18,6 @@ if __name__ == '__main__':
     #drop thes 2 columns to get only the columns with the subjectcs
     df1 = df1.drop("Student_ID")
     df1 = df1.drop("University")
-    print(df1.columns)
     #appending to a list the column names
     names = [[]]
     for name in df1.columns:
@@ -26,7 +26,7 @@ if __name__ == '__main__':
     #summing all gradse the students got for all the subjects
     df = df.transform(lambda dframe: dframe.withColumn('total', sum(dframe[col] for col in names.values)))
     #calculating the student average
-    df2 = df.transform(lambda dframe: dframe.withColumn('GPA', dframe['total'].cast('float') / len(df1)))
+    df2 = df.transform(lambda dframe: dframe.withColumn('GPA', dframe['total'].cast('float') / len(df1.columns)))
     #selecting the student attributes and the university
     df3 = df2.transform(lambda dframe: dframe.select('student_ID', 'total', 'GPA', dframe["University"]))
     #grouping all the students and the university and grouping by University name (expensive) ->to get Uni score
@@ -35,5 +35,3 @@ if __name__ == '__main__':
     elapsedTime = str(time.time() - time_start)#calculate the time elapsed
     print("The top 5 Universities are ranked as following: ", df5.show(5))#printing the top 5 universities
     print("The elapsed time for this algorithm is ", elapsedTime)#print time elapsed
-
-
