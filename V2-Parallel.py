938cad78273868978871f966cfe102b79216404c
Sny <semidaandreicha@gmail.com>
2019-01-18 14:54:13 +0100

    #broken

--- V2-Parallel.py
+++ V2-Parallel.py
@@ -4,11 +4,15 @@ from pyspark.sql import SparkSession, DataFrame
 
 def transform(self, f):
     return f(self)
+    
 DataFrame.transform = transform
+
 if __name__ == '__main__':
     #initializing spark objects
-    spark = SparkSession.builder.master("local[*]").appName("University Ranking").getOrCreate()
+    spark = SparkSession.builder.master("local[4]").appName("University Ranking").getOrCreate()
     sparkContext = spark.sparkContext
+    print("Parallel implemenation running with 4 Cores")
+
     #importing/reading all csv files
     df = spark.read.options(header='true', inferschema='true', delimiter=',').csv("data/*.csv")  # type: DataFrame
     #start time
@@ -18,13 +22,10 @@ if __name__ == '__main__':
     #drop thes 2 columns to get only the columns with the subjectcs
     df1 = df1.drop("student_ID")
     df1 = df1.drop("University")
-    #appending to a list the column names
-    names = [[]]
-    for name in df1.columns:
-        names.append(name)	
+    
     # Calculating the University score based on the students's average
     #summing all gradse the students got for all the subjects
-    df = df.transform(lambda dframe: dframe.withColumn('total', sum(dframe[col] for col in names.values)))
+    df = df.transform(lambda dframe: dframe.withColumn('total', sum(dframe[col] for col in df1.columns)))
     #calculating the student average
     df2 = df.transform(lambda dframe: dframe.withColumn('GPA', dframe['total'].cast('float') / len(df1.columns)))
     #selecting the student attributes and the university
@@ -33,5 +34,6 @@ if __name__ == '__main__':
     df4 = df3.transform(lambda dframe: dframe.groupBy(dframe["University"]).agg(fn.avg('GPA').alias('Score')))
     df5 = df4.transform(lambda dframe: dframe.sort(dframe['Score'].desc()))#Ranking the universities based on their score
     elapsedTime = str(time.time() - time_start)#calculate the time elapsed
-    print("The top 5 Universities are ranked as following: ", df5.show(5))#printing the top 5 universities
+    print("The top 5 Universities are ranked as following: ")
+    df5.show(5)#printing the top 5 universities
     print("The elapsed time for this algorithm is ", elapsedTime)#print time elapsed
